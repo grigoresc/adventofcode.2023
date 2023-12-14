64f8a45dbce5ae22e847335bccab514a983106f4
Ciprian Grigore <grigoresc@outlook.com>
2023-12-14 12:50:17 +0200

    day 14

--- day14/Program.cs
+++ day14/Program.cs
@@ -1,39 +1,15 @@
-﻿
-using System.ComponentModel.DataAnnotations;
-using System.Runtime.CompilerServices;
-using aoc.common;
-
-//var lines = File.ReadAllLines("sample.txt");
+﻿//var lines = File.ReadAllLines("sample.txt");
 var lines = File.ReadAllLines("input.txt");
 
 var map = lines
     .Select(x => x.ToArray()).ToArray();
 
-/*
-O....#....
-O.OO#....#
-.....##...
-OO.#O....O
-.O.....O#.
-O.#..O.#.#
-..O..#O..O
-.......O..
-#....###..
-#OO..#....
-
- 0 is rock
- # is blocked
-    . is empty
- */
 var len = map.Length;
-print(map);
 MoveNorth(map);
-print(map);
 var sln1 = map.Select((line, idx) => line.Where(c => c == 'O').Count() * (len - idx)).Sum();
 Console.WriteLine(sln1);//102497
 
 var cycles = 1000000000;
-//var cycles = 3;
 
 var cmap = map = lines
     .Select(x => x.ToArray()).ToArray();
@@ -45,11 +21,7 @@ var skipCalc = false;
 var sln2 = 0;
 for (int c = 0; c < cycles; c++)
 {
-    //Console.WriteLine(c);
     cmap = Cycle(cmap);
-
-
-    //print(cmap);
     sln2 = cmap.Select((line, idx) => line.Where(c => c == 'O').Count() * (len - idx)).Sum();
 
     var hash = cmap.Select(
@@ -57,11 +29,9 @@ for (int c = 0; c < cycles; c++)
             (c, jdx) => c.ToString() + idx.ToString() + jdx.ToString())
         ).SelectMany(x => x).Aggregate((x, y) => x + y);
 
-    Console.WriteLine($"{c} {sln2}");
     if (!skipCalc)
         if (lst.ContainsKey(hash))
         {
-            Console.WriteLine($"we have a repeat at {c}");
             if (!rep)
             {
                 rep = true;
@@ -77,25 +47,20 @@ for (int c = 0; c < cycles; c++)
                 var repeatSize = lastRepSize - firstRepSize;
                 var repeatCount = (cycles - firstRepSize) / repeatSize;
                 var nextc = firstRepSize + (repeatCount * repeatSize);
-                Console.WriteLine($"nextc {nextc}");
                 c = nextc;
                 skipCalc = true;
 
-                //break;
             }
-            //c = (cycles / c) * c;
-            //break;
         }
         else
         {
             lst.Add(hash, (c, sln2));
         }
 
-    //Console.WriteLine($"{c} {sln} {hash}");
-
 }
 
 Console.WriteLine(sln2);//105008
+
 //print map
 void print(char[][] map, string? title = "")
 {
@@ -106,31 +71,17 @@ void print(char[][] map, string? title = "")
     }
 
 }
-//NWSE
 
 char[][] Cycle(char[][] map)
 {
     MoveNorth(map);
-    //print(map, "N");
-
     map = Rotate(map);
-    //print(map, "R");
     MoveNorth(map);
-    //print(map, "N");
-
     map = Rotate(map);
-    //print(map, "R");
     MoveNorth(map);
-    //print(map, "N");
-
     map = Rotate(map);
-    //print(map, "R");
     MoveNorth(map);
-    //print(map, "N");
-
     map = Rotate(map);
-    //print(map, "R");
-
     return map;
 }
 
@@ -149,13 +100,11 @@ char[][] Rotate(char[][] map)
     return newmap;
 }
 
-
 void MoveNorth(char[][] map)
 {
     for (int j = 0; j < len; j++)
     {
         var rocks = 0;
-        //var isblocked = false;
         var start = -1;
         for (int i = 0; i < len; i++)
         {
