5d5166514ba681ca6b50b0475a4bd9b8a6c1af46
Ciprian Grigore <grigoresc@outlook.com>
2023-12-14 17:42:30 +0200

    day 12 1st part

---
+++ day12/Solve2.cs
@@ -0,0 +1,17 @@
+﻿namespace day12;
+
+public partial class Solve
+{
+    [Theory]
+    [InlineData("sample.txt", 525152)]
+    //[InlineData("input.txt", 7843)]
+    void Part2(string input, int expected)
+    {
+        var map = Read(input);
+        cnt = 0;
+        foreach (var line in map)
+            calc(line.Springs, line.Lengths.ToArray());
+        cnt.Dump().AssertSolved(expected);
+    }
+}
+
