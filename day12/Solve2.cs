e43703377391f3ac7176f08756057fde3f9f7836
Ciprian Grigore <grigoresc@outlook.com>
2023-12-15 21:03:47 +0200

    day 12 part2 solved

--- day12/Solve2.cs
+++ day12/Solve2.cs
@@ -4,27 +4,24 @@ namespace day12;
 public partial class Solve
 {
     [Theory]
-    [InlineData("sample.txt", 1, 21)]
-    [InlineData("input.txt", 1, 7843)]
-    [InlineData("?.### 1,3", 1, 1)]
-    [InlineData("?###???????? 3,2,1", 1, 10)]
+    [InlineData("sample.txt", 1, 21L)]
+    [InlineData("input.txt", 1, 7843L)]
+    [InlineData("?.### 1,3", 1, 1L)]
+    [InlineData("?###???????? 3,2,1", 1, 10L)]
 
-    [InlineData("sample.txt", 5, 525152)]
-    [InlineData(".??..??...?##. 1,1,3", 5, 16384)]
+    [InlineData("sample.txt", 5, 525152L)]
+    [InlineData(".??..??...?##. 1,1,3", 5, 16384L)]
 
-    [InlineData("input.txt", 5, 525152)]
-    //[InlineData("sample.txt", 21)]
-    //[InlineData("input.txt", 7843)]
-    //[InlineData("input.txt", 7843)]
-    void Part2(string input, int repeat, int expected)
+    [InlineData("input.txt", 5, 10153896718999L)]
+    void Part2(string input, int repeat, long expected)
     {
         var map = Read(input);
-        cnt = 0;
+        var sln2 = 0L;
         foreach (var line in map)
         {
             //repeat a char[]
             var s = line.Springs.ToCharArray();
-            for (var i = 0; i < repeat-1; i++)
+            for (var i = 0; i < repeat - 1; i++)
             {
                 s = s.Concat(new char[] { '?' }).ToArray();
                 s = s.Concat(line.Springs.ToCharArray()).ToArray();
@@ -35,49 +32,47 @@ public partial class Solve
                 l = l.Concat(line.Lengths.ToArray()).ToArray();
             }
 
-            line.Springs.Dump("compute");
-            calc2(
+            sln2 += calc2(
                 s,
                 l,
                 "");
-            cnt.Dump("cnt");
         }
-        cnt.Dump().AssertSolved(expected);
+        sln2.Dump().AssertSolved(expected);
     }
 
-    private void calc2(char[] springs, int[] lengths, string sofar)
+    string key(char[] springs, int[] lengths)
     {
-        //"".Dump("-------------------");
-        //springs.Dump("calc2");
-        //lengths.Dump("lengths");
+        return $"{new string(springs)}|{string.Join(",", lengths)}";
+    }
 
-        //sofar.Dump("sofar");
+    //memoize calc2
+    private Dictionary<string, long> memo = new();
 
+    private long calc2(char[] springs, int[] lengths, string sofar)
+    {
+        var hash = key(springs, lengths);
+        if (memo.ContainsKey(hash))
+        {
+            return memo[hash];
+        }
+        
+        var total = 0L;
 
         if (springs.Length > 0 && springs[0] == '.')
         {
-            calc2(springs[1..], lengths, sofar + ".");
-            return;
+            total += calc2(springs[1..], lengths, sofar + ".");
+            return total;
         }
 
         if (springs.Length == 0 && lengths.Length == 0)
         {
-            //$"{sofar} zero".Dump();
-
-            cnt++;
-
-            return;
+            total++;
+            return total;
         }
 
-        //if (lengths.Length == 0)
-        //{
-        //    return;
-        //}
-        //if is # then it's damaged; if it's . then it's not
-        //var damagedString = lengths[0];
-        //we build a list of # with length=len (where # is actualy a damaged spring)
         var damagedStrignStarted = false;
         var len = 0;
+
         //find the next sure thing (not damaged one)
         var idxO = springs.Select((c, idx) => new { c, idx }).Where(c => c.c == '.').FirstOrDefault();
 
@@ -111,13 +106,13 @@ public partial class Solve
                 //1. compute as if we had a . here
                 if (!damagedStrignStarted)
                 {
-                    calc2(springs[(i + 1)..], lengths, sofar + ".");
+                    total += calc2(springs[(i + 1)..], lengths, sofar + ".");
                 }
                 else
                 {
                     if (lengths.Length > 0 && len == lengths[0])
                     {
-                        calc2(springs[(i + 1)..], lengths[1..], sofar + $"(#{len}).");
+                        total += calc2(springs[(i + 1)..], lengths[1..], sofar + $"(#{len}).");
                     }
                     else
                     {
@@ -137,20 +132,19 @@ public partial class Solve
         {
             if (lengths.Length == 1)
             {
-                calc2(springs[(idx)..], new int[0], sofar + $"(#{len})N");
-                //lengths[0].Dump("lengths[0]");
-                //$"{sofar}(#{len}!)".Dump();
-                //cnt++;
-                //return;
+                total += calc2(springs[(idx)..], new int[0], sofar + $"(#{len})N");
             }
             else
-                calc2(springs[idx..], lengths[1..], sofar + "G");
+                total += calc2(springs[idx..], lengths[1..], sofar + "G");
         }
         else
         {
             //do nothing
         }
 
+        memo[hash] = total;
+
+        return total;
     }
 }
 
